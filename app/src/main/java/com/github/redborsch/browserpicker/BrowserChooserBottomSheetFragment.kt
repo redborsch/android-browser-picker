5da2b50e3b1c4cfe20b675c071167f8fcfd3367e
Mike Mostovyi <8543807+MikeMostovyi@users.noreply.github.com>
2026-02-14 22:19:15 +0100

    Better edge-to-edge support

--- app/src/main/java/com/github/redborsch/browserpicker/BrowserChooserBottomSheetFragment.kt
+++ app/src/main/java/com/github/redborsch/browserpicker/BrowserChooserBottomSheetFragment.kt
@@ -1,38 +1,57 @@
 package com.github.redborsch.browserpicker
 
+import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.DialogInterface
 import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION_CODES
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.view.WindowInsets
 import androidx.core.net.toUri
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import com.github.redborsch.browserpicker.databinding.FragmentBrowserChooserBinding
-import com.github.redborsch.browserpicker.shared.fragment.ViewBindingFragment
-import com.github.redborsch.browserpicker.shared.fragment.attachBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.internal.EdgeToEdgeUtils
 
-class BrowserChooserBottomSheetFragment :
-    BottomSheetDialogFragment(),
-    ViewBindingFragment<FragmentBrowserChooserBinding> {
+class BrowserChooserBottomSheetFragment : BottomSheetDialogFragment() {
 
     private val viewModel: BrowserChooserViewModel by activityViewModels()
 
     private var destroyingDialog = false
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View = attachBinding(
-        FragmentBrowserChooserBinding.inflate(inflater, container, false)
-    )
+    @SuppressLint("RestrictedApi", "WrongConstant")
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return BottomSheetDialog(requireContext(), theme).apply {
+            setContentView(R.layout.fragment_browser_chooser)
 
-    override fun FragmentBrowserChooserBinding.setUp() {
-        BrowserChooserUiHelper(viewModel)
-            .setUp(requireActivity(), this, lifecycleScope, retrieveUri())
+            // Edge-to-edge logic was borrowed from
+            // https://github.com/material-components/material-components-android
+            EdgeToEdgeUtils.applyEdgeToEdge(window!!, true)
+
+            ViewCompat.setOnApplyWindowInsetsListener(window!!.decorView) { v, insets ->
+                val leftInset: Int
+                val rightInset: Int
+                if (Build.VERSION.SDK_INT >= VERSION_CODES.R) {
+                    leftInset = insets.getInsets(WindowInsets.Type.systemBars()).left
+                    rightInset = insets.getInsets(WindowInsets.Type.systemBars()).right
+                } else {
+                    leftInset = insets.stableInsetLeft
+                    rightInset = insets.stableInsetRight
+                }
+
+                v.setPadding(leftInset, 0, rightInset, 0)
+                insets
+            }
+
+            val root = findViewById<View>(R.id.chooser_root)!!
+            val binding = FragmentBrowserChooserBinding.bind(root)
+            binding.setUp()
+        }
     }
 
     override fun onDestroyView() {
@@ -54,8 +73,13 @@ class BrowserChooserBottomSheetFragment :
         }
     }
 
+    private fun FragmentBrowserChooserBinding.setUp() {
+        BrowserChooserUiHelper(viewModel)
+            .setUp(requireActivity(), this, lifecycleScope, retrieveUri())
+    }
+
     private fun retrieveUri(): Uri =
         requireActivity().intent.data
-            // Should not happen!
+        // Should not happen!
             ?: "https://mozilla.org".toUri()
 }
