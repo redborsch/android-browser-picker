9554fb497d1ba95fa424fe59c6d9f81a1512141b
Mike Mostovyi <8543807+MikeMostovyi@users.noreply.github.com>
2026-02-28 01:01:26 +0100

    Fixed hardcoded plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.android.library") version "8.9.1" apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.library) apply false
 }
