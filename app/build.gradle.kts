3085b37405352b83409e4d6e448f1b37c5c223f0
Mike Mostovyi <8543807+MikeMostovyi@users.noreply.github.com>
2026-03-01 14:21:01 +0100

    Version 2.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.github.redborsch.browserpicker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.0.1"
+        versionCode = 5
+        versionName = "2.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
