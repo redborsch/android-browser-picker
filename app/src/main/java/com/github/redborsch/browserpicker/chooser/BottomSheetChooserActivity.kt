eab747a3bd196dcdd62781403e02fb66cfe7a599
Mike Mostovyi <8543807+MikeMostovyi@users.noreply.github.com>
2026-03-26 15:40:15 +0100

    Fixed visual artifacts on some devices when showing picker bottom sheet

--- app/src/main/java/com/github/redborsch/browserpicker/chooser/BottomSheetChooserActivity.kt
+++ app/src/main/java/com/github/redborsch/browserpicker/chooser/BottomSheetChooserActivity.kt
@@ -1,14 +1,26 @@
 package com.github.redborsch.browserpicker.chooser
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.github.redborsch.fragment.defaultFragmentTag
+import com.github.redborsch.fragment.showDialog
 
 class BottomSheetChooserActivity : AbstractChooserActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (savedInstanceState == null) {
-            BrowserChooserBottomSheetFragment().show(supportFragmentManager, null)
+        // On some devices and in some situations showing the dialog fragment right away causes
+        // artifacts.
+        // For example, on Galaxy Tab A8 (Android 14) when sharing URL from Chrome.
+        Handler(Looper.getMainLooper()).post {
+            if (!isFinishing) {
+                supportFragmentManager.showDialog(defaultFragmentTag) {
+                    BrowserChooserBottomSheetFragment()
+                }
+
+            }
         }
     }
 }
